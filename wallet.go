3e7c01d9f45ec4393c9e0b483e80a0ef99c7c62a
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-04-05 21:32:29 +0200

    transaction to db

--- wallet.go
+++ wallet.go
@@ -12,29 +12,64 @@ type Wallet struct {
 	Balance int `json:"balance"`
 }
 
-func main() {
-
-	db, err := sql.Open("mysql", "dilawar:passord123@tcp(127.0.0.1:3306)/wallet_service")
-
+func errorHandler(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func main() {
+	// Disse verdiene skal servicen få fra Coordinator via TCP
+	// TODO: Sette opp TCP server for å hente verdier fra Coordinator
+
+	user_id := 1
+	price := 50
+	transaction_id := 69
+
+	db, err := sql.Open("mysql", "dilawar:passord123@tcp(127.0.0.1:3306)/wallet_service")
+	errorHandler(err)
 
 	defer db.Close()
 
-	results, err := db.Query("SELECT * FROM wallet")
-	if err != nil {
-		panic(err.Error())
-	}
+	results, err := db.Query("SELECT * FROM wallet WHERE user_id=?", user_id)
+	errorHandler(err)
 
+	var wallet Wallet
 	for results.Next() {
-		var wallet Wallet
 
 		err = results.Scan(&wallet.User_id, &wallet.Balance)
 		if err != nil {
 			panic(err.Error())
 		}
-
-		fmt.Println(wallet)
 	}
+	fmt.Println("Wallet :", wallet)
+
+	tx, err := db.Begin()
+	errorHandler(err)
+	res, err := tx.Exec("UPDATE wallet SET balance=? WHERE user_id=?", wallet.Balance-price, user_id)
+	errorHandler(err)
+
+	if wallet.Balance-price >= 0 {
+		fmt.Println("OK PREP", transaction_id) // skal sendes til Coordinator
+
+		if err != nil {
+			tx.Rollback()
+			panic(err.Error())
+		}
+
+		rows, err := res.RowsAffected()
+		errorHandler(err)
+
+		fmt.Println("Rows :", rows)
+
+		errorHandler(tx.Commit())
+
+		fmt.Println("OK COMMIT", transaction_id) // skal sendes til Coordinator
+
+	} else {
+		tx.Rollback()
+		panic(err.Error())
+	}
+
+	fmt.Println("Done.")
 }
