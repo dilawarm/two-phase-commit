588d9596bc3e75bf4755d0e484a3e726ca49ba55
haavasma <haavasma@stud.ntnu.no>
2020-04-06 15:38:16 +0200

    order.go

--- order/order.go
+++ order/order.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -15,7 +20,7 @@ const (
 
 type Order struct {
 	User_id int `json:"user_id"`
-	Amount  int `json:"balance"`
+	Amount  int `json:"amount"`
 }
 
 func errorHandler(err error) {
@@ -24,50 +29,73 @@ func errorHandler(err error) {
 	}
 }
 
-
-func handlePrepare(conn net.Conn, password string) *sql.Tx {
-	p:= make([]byte, 2048)
+func handlePrepare(conn net.Conn, password string) *sql.Tx {
+	p := make([]byte, 2048)
 	_, err := conn.Read(p)
 
 	if err != nil {
 		fmt.Println("Error reading: ", err.Error())
 	}
-	message := string(p)
+	message := string(p[:2048])
 	temp := strings.Split(message, " ")
 	user_id, _ := strconv.Atoi(temp[0])
-	amount, _:= strconv.Atoi(temp[1])
-
-	for i := 0; i<amount; i++ {
-
-	}
+	amount, _ := strconv.Atoi(temp[1])
 
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
-	data, err := ioutil.ReadFile("../.config")
-	if err != nil {
-	  fmt.Println("File reading error", err)
-	}
-	fmt.Println("Contents of file:")
-	fmt.Println(string(data))
-	password := string(data)
 
-	db, err:=sql.Open("mysql", "haavasma:")
+	db, err := sql.Open("mysql", "haavasma:"+password+"@tcp(127.0.0.1:3306)/haavasma")
+	errorHandler(err)
+	defer db.Close()
+
+	var order Order
+	order.Amount = amount
+	order.User_id = user_id
+
+	tx, err := db.Begin()
+	errorHandler(err)
+	res, err := tx.Exec("INSERT INTO order (order_id, user_id, amount) VALUES (DEFAULT, ?, ?)", user_id, amount)
+	fmt.Println(res.RowsAffected())
+	errorHandler(err)
+
+	if err != nil {
+		tx.Rollback()
+		panic(err.Error())
+	}
+	conn.Write([]byte("1"))
+	conn.Close()
+	return tx
 }
 
 func main() {
 	data, err := ioutil.ReadFile("../.config")
 	if err != nil {
 		fmt.Println("File reading error", err)
-		os.Exit(data)
+		os.Exit(1)
 	}
 	fmt.Println("Contents of file:")
 	fmt.Println(string(data))
-	password:= string(data)
+	password := string(data)
 
-	1, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	socket, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening: ", err.Error())
 		os.Exit(1)
+	}
+	defer socket.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	prepared := false
+	var tx *sql.Tx
+	for {
+		conn, err := socket.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		if !prepared {
+			tx = handlePrepare(conn, password)
+			prepared = true
+		}
 	}
 }
