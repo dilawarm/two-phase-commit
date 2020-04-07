c2f7c336ab7c59169763eeff444ca2db0e89e015
emir derouiche <memir0@hotmail.no>
2020-04-07 12:18:47 +0200

    Improved error handeling

--- orchestrator.rs
+++ orchestrator.rs
@@ -1,14 +1,14 @@
 use std::thread;
-use std::collections::HashMap;
+//use std::collections::HashMap;
 use std::sync::{Arc, Condvar, Mutex};
 use std::net::{TcpStream, TcpListener, IpAddr, Ipv4Addr, SocketAddr};
 use std::time::Duration;
 use std::io::prelude::*;
 
-const wallet_ms_ip: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
-const wallet_ms_port: u16 = 3333u16;
-const order_ms_ip: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
-const order_ms_port: u16 = 3334u16;
+const WALLET_MS_IP: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
+const WALLET_MS_PORT: u16 = 3333u16;
+const ORDER_MS_IP: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
+const ORDER_MS_PORT: u16 = 3334u16;
 
 fn main() {
     let mut threads = Vec::new();
@@ -20,64 +20,135 @@ fn main() {
         //let transaction_queue = HashMap::new();
         threads.push(thread::Builder::new().name("coordinator".to_string()).spawn(
             move || {
-            handle_request(stream);
+                let mut tries = 0;
+                while !handle_request(&stream) && tries < 5 {
+                    tries += 1;
+                }
         }));
     }
 }
 
-fn handle_request(mut client_stream: TcpStream) {
+fn handle_request(mut _client_stream: &TcpStream) -> bool {
     // TCP connection duration before timeout
     let timeout = Duration::from_millis(5000);
 
     // Wallet micro service preperation
     let account = 1u32;
     let amount = 100i32;
-    let wallet_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(wallet_ms_ip[0], wallet_ms_ip[1], wallet_ms_ip[2], wallet_ms_ip[3])), wallet_ms_port);
+    let wallet_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(WALLET_MS_IP[0], WALLET_MS_IP[1], WALLET_MS_IP[2], WALLET_MS_IP[3])), WALLET_MS_PORT);
     let mut wallet_stream = match TcpStream::connect_timeout(&wallet_socket, timeout) {
         Ok(stream) => stream,
         Err(e) => {
             println!("Error: {}", e);
-            return;
+            return false;
         }
     };
-    wallet_stream.write(&account.to_be_bytes());
-    wallet_stream.write(&amount.to_be_bytes());
+    match wallet_stream.write(&account.to_be_bytes()){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
+    match wallet_stream.write(&amount.to_be_bytes()){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
 
     // Order micro service preperation
     let user_id = 1u32;
     let amount_of_items = 5u32;
     let items = [1u32, 2u32, 3u32, 4u32, 5u32];
 
-    let order_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(order_ms_ip[0], order_ms_ip[1], order_ms_ip[2], order_ms_ip[3])), order_ms_port);
+    let order_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(ORDER_MS_IP[0], ORDER_MS_IP[1], ORDER_MS_IP[2], ORDER_MS_IP[3])), ORDER_MS_PORT);
     let mut order_stream = match TcpStream::connect_timeout(&order_socket, timeout) {
         Ok(stream) => stream,
         Err(e) => {
             println!("Error: {}", e);
-            return;
+            return false;
         }
     };
-    order_stream.write(&user_id.to_be_bytes());
-    order_stream.write(&amount_of_items.to_be_bytes());
+    match order_stream.write(&user_id.to_be_bytes()){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
+    match order_stream.write(&amount_of_items.to_be_bytes()){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
     for i in 0..amount_of_items {
-        order_stream.write(&items[i as usize].to_be_bytes());
+        match order_stream.write(&items[i as usize].to_be_bytes()){
+            Ok(_result) => {},
+            Err(e) => {
+                println!("Error: {}", e);
+                return false;
+            }
+        };
     }
 
     // Read response
     let mut wallet_response = [0u8];
     let mut order_response = [0u8];
-    wallet_stream.read(&mut wallet_response);
-    order_stream.read(&mut order_response);
+    match wallet_stream.read(&mut wallet_response){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
+    match order_stream.read(&mut order_response){
+        Ok(_result) => {},
+        Err(e) => {
+            println!("Error: {}", e);
+            return false;
+        }
+    };
 
     if order_response[0] == 1 && wallet_response[0] == 1 {
         // Both services ready, go ahead and commit
         let commit_message = [1u8];
-        wallet_stream.write(&commit_message);
-        order_stream.write(&commit_message);
+        match wallet_stream.write(&commit_message){
+            Ok(_result) => {},
+            Err(e) => {
+                println!("Error: {}", e);
+                return false;
+            }
+        };
+        match order_stream.write(&commit_message){
+            Ok(_result) => {},
+            Err(e) => {
+                println!("Error: {}", e);
+                return false;
+            }
+        };
+        return true;
     }
     else {
         // Error, rollback
         let rollback_message = [2u8];
-        wallet_stream.write(&rollback_message);
-        order_stream.write(&rollback_message);
+        match wallet_stream.write(&rollback_message){
+            Ok(_result) => {},
+            Err(e) => {
+                println!("Error: {}", e);
+                return false;
+            }
+        };
+        match order_stream.write(&rollback_message){
+            Ok(_result) => {},
+            Err(e) => {
+                println!("Error: {}", e);
+                return false;
+            }
+        };
+        return false;
     }
 }
