c7932e19188a80c22bdc2496b6ee1870443c1721
haavasma <haavasma@stud.ntnu.no>
2020-04-12 19:57:54 +0200

    testing database connection

--- microservices/order.go
+++ microservices/order.go
@@ -45,8 +45,7 @@ func handlePrepare(conn net.Conn, password string) micro.Prep {
 	}
 	list.List[user_id] = true
 	list.Mux.Unlock()
-	fmt.Println(password[len(password)-1])
-	db, err := sql.Open("mysql", password+"@tcp(localhost:3306)/order_service")
+	db, err := sql.Open("mysql" /*password+*/, "dilawar:passord123@tcp(localhost:3306)/order_service")
 	if err != nil {
 		return micro.Prep{4, nil, user_id}
 	}
